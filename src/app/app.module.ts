18867c75fcb3d8022c0e281748c46b341da1e073
Heera Babu <heerababu80@gmail.com>
2023-04-11 22:54:31 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
+import { HttpClientModule } from '@angular/common/http';  
+import {DataTablesModule} from 'angular-datatables'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    StudentListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  imports: [  
+    BrowserModule,  
+    AppRoutingModule,  
+    FormsModule,  
+    ReactiveFormsModule,  
+    HttpClientModule,  
+    DataTablesModule  
+  ],  
+  providers: [],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  
---
+++ src/app/student.ts
@@ -0,0 +1,6 @@
+export class Student {  
+    student_id:number=0;  
+    student_name:String="";  
+    student_email:String="";  
+    student_branch:String="";  
+}  
